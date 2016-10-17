3a3496a72f6fdc05d2ea1a181f5a5245c644f98c
MarianaPacheco <pachecoramirez.mariana@gmail.com>
2016-10-17 13:32:07 -0500

    Agregué código de js al archivo de traducción formulario

--- bicis-locas/bicis-locas/js/main.js
+++ bicis-locas/bicis-locas/js/main.js
@@ -1,3 +1,9 @@
 function validateForm(){
 	/* Escribe tú código aquí */
 }
+
+function usarLetras(e)
+{
+	key*e.keyCode || e.which;
+	teclado*String 
+}
---
+++ traduccion-formulario/js/main.js
@@ -0,0 +1,32 @@
+function cambiarIdioma(idioma) {
+	var elTitle = document.getElementById('form-signin-heading');
+	var inputEmail = document.getElementById('inputEmail');
+	var inputPassword = document.getElementById('inputPassword');
+	//var elCheck = document.getElementById('checkRemember');
+	var elBtnSign = document.getElementById('btnSign');
+
+	if (idioma == "es"){
+		elTitle.innerHTML = "Por favor regístrate"; 
+		inputEmail.setAttribute('placeholder', 'Introduce tu email');
+		inputPassword.setAttribute('placeholder', 'Contraseña');
+		//elCheck.innerHTML"Recordar contraseña";
+		elBtnSign.innerHTML = "Regístrate"; 
+	} else {
+		elTitle.innerHTML = "Please sign in"; 
+		inputEmail.setAttribute('placeholder', 'Please enter email');
+		inputPassword.setAttribute('placeholder', 'Password');
+		//elCheck.innerHTML"Remember me";
+		elBtnSign.innerHTML = "Sign in"; 
+	}
+}
+
+cambiarIdioma("es");
+
+var btnEs = document.getElementById("btnEs");
+btnEs.onclick = function() {	/*function() es una función anónima*/
+	cambiarIdioma("es");
+};
+var btnEn = document.getElementById("btnEn");
+btnEn.onclick = function() {	/*function() es una función anónima*/
+	cambiarIdioma("en");
+}
